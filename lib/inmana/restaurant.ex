25ddc34b919c4499fc5fdff042dd2936c439e5f0
Mariana Faria <marianafaria03@hotmail.com>
2021-04-21 20:56:10 -0300

    Implementando os supplies

--- lib/inmana/restaurant.ex
+++ lib/inmana/restaurant.ex
@@ -2,6 +2,8 @@ defmodule Inmana.Restaurant do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Inmana.Supply
+
   @primary_key {:id, :binary_id, autogenerate: true}
 
   @required_params [:email, :name]
@@ -12,13 +14,15 @@ defmodule Inmana.Restaurant do
     field :email, :string
     field :name, :string
 
+    has_many :supplies, Supply
+
     timestamps()
   end
 
   def changeset(params) do
     %__MODULE__{}
-    |> cast(params, [:email, :name])
-    |> validate_required([:email, :name])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
     |> validate_length(:name, min: 2)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
